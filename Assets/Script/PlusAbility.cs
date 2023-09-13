d8f51876ef8e0438ae43701c110a8643bf54b6e6
kjg5370 <kjk03024@naver.com>
2023-09-13 17:19:14 +0900

    능력치 골드 부여

--- Assets/Script/PlusAbility.cs
+++ Assets/Script/PlusAbility.cs
@@ -8,26 +8,60 @@ public class PlusAbility : MonoBehaviour
     public TMP_Text ballDamage;
     public TMP_Text ballSpeed;
     public TMP_Text paddleSpeed;
+
+    public TMP_Text ballDamagePrice;
+    public TMP_Text ballSpeedPrice;
+    public TMP_Text paddleSpeedPrice;
+
+    int bDamagePrice;
+    int bSpeedPrice;
+    int pSpeedPrice;
     void Start()
     {
         ballDamage.text = DataManager.DMinstance.ballDamage.ToString();
         ballSpeed.text = DataManager.DMinstance.ballSpeed.ToString();
         paddleSpeed.text = DataManager.DMinstance.paddleSpeed.ToString();
+
+        bDamagePrice = 100;
+        bSpeedPrice = 100;
+        pSpeedPrice = 100;
+
+        ballDamagePrice.text = bDamagePrice.ToString();
+        ballSpeedPrice.text = bSpeedPrice.ToString();
+        paddleSpeedPrice.text = pSpeedPrice.ToString();
     }
 
     public void ballDamageBtn()
     {
-        DataManager.DMinstance.ballDamage += 1;
-        ballDamage.text = DataManager.DMinstance.ballDamage.ToString();
+        if (DataManager.DMinstance.gold > bDamagePrice)
+        {
+            DataManager.DMinstance.ballDamage += 1;
+            ballDamage.text = DataManager.DMinstance.ballDamage.ToString();
+            DataManager.DMinstance.gold -= bDamagePrice;
+            bDamagePrice += 100;
+            ballDamagePrice.text = bDamagePrice.ToString();
+        }
     }
     public void ballSpeedBtn()
     {
-        DataManager.DMinstance.ballSpeed += 0.5f;
-        ballSpeed.text = DataManager.DMinstance.ballSpeed.ToString();
+        if (DataManager.DMinstance.gold > bSpeedPrice)
+        {
+            DataManager.DMinstance.ballSpeed += 0.5f;
+            ballSpeed.text = DataManager.DMinstance.ballSpeed.ToString();
+            DataManager.DMinstance.gold -= bSpeedPrice;
+            bSpeedPrice += 100;
+            ballSpeedPrice.text = bSpeedPrice.ToString();
+        }
     }
     public void paddleSpeedBtn()
     {
-        DataManager.DMinstance.paddleSpeed += 0.5f;
-        paddleSpeed.text = DataManager.DMinstance.paddleSpeed.ToString();
+        if (DataManager.DMinstance.gold > pSpeedPrice)
+        {
+            DataManager.DMinstance.paddleSpeed += 0.5f;
+            paddleSpeed.text = DataManager.DMinstance.paddleSpeed.ToString();
+            DataManager.DMinstance.gold -= pSpeedPrice;
+            pSpeedPrice += 100;
+            ballSpeedPrice.text = pSpeedPrice.ToString();
+        }
     }
 }
